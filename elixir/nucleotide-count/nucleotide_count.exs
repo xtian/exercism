a60099d3990a800696faa83e04fd5a439fc5c535
Christian Wesselhoeft <hi@xtian.us>
2016-09-06 14:26:06 -0600

    Improve Elixir nucleotide-count

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -13,15 +13,11 @@ defmodule NucleotideCount do
   1
   """
   @spec count([char], char) :: non_neg_integer
+  def count(_, char) when not char in @nucleotides, do: raise ArgumentError
   def count(strand, nucleotide) do
-    validate_nucleotide!(nucleotide)
-
-    strand
-    |> Enum.map(&validate_nucleotide!/1)
-    |> Enum.filter(&(&1 == nucleotide))
-    |> Enum.count()
+    validate_strand!(strand)
+    Enum.count(strand, &(&1 == nucleotide))
   end
-
 
   @doc """
   Returns a summary of counts by nucleotide.
@@ -33,20 +29,15 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
+    validate_strand!(strand)
     output = Map.new(@nucleotides, &({&1, 0}))
 
-    try do
-      Enum.reduce strand, output, fn x, acc ->
-        Map.update!(acc, x, &(&1 + 1))
-      end
-    rescue
-      KeyError -> raise ArgumentError
+    Enum.reduce strand, output, fn x, acc ->
+      Map.update!(acc, x, &(&1 + 1))
     end
   end
 
-  @spec validate_nucleotide!(char) :: boolean
-  defp validate_nucleotide!(nucleotide) do
-    unless Enum.member?(@nucleotides, nucleotide), do: raise ArgumentError
-    nucleotide
+  defp validate_strand!(strand) do
+    Enum.any?(strand, &(not &1 in @nucleotides)) and raise ArgumentError
   end
 end
