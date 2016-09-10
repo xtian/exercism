073f2b8976a1393aac8b73a5c70fead1a6728a37
Christian Wesselhoeft <hi@xtian.us>
2016-09-10 17:41:28 -0600

    Complete Elixir bracket-push

---
+++ elixir/bracket-push/bracket_push.exs
@@ -0,0 +1,22 @@
+defmodule BracketPush do
+  @doc """
+  Checks that all the brackets and braces in the string are matched correctly, and nested correctly
+  """
+  @spec check_brackets(String.t) :: boolean
+  def check_brackets(string) do
+    try do
+      string |> to_charlist() |> Enum.reduce([], &process_symbol/2) == []
+    rescue
+      FunctionClauseError -> nil
+    end
+  end
+
+  @spec process_symbol(char, [:atom]) :: [:atom]
+  defp process_symbol(?{, open), do: [:brace | open]
+  defp process_symbol(?[, open), do: [:bracket | open]
+  defp process_symbol(?(, open), do: [:paren | open]
+  defp process_symbol(?}, [:brace | open]), do: open
+  defp process_symbol(?], [:bracket | open]), do: open
+  defp process_symbol(?), [:paren | open]), do: open
+  defp process_symbol(char, open) when not char in '{}[]()', do: open
+end
