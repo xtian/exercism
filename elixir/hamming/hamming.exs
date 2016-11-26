ba812fdfa671a99b7d1f8177c4b46667bc3e6711
Christian Wesselhoeft <hi@xtian.us>
2016-11-26 08:58:36 -0700

    Improve Elixir sieve
    Passing the accumulator as a parameter enables tail recursion optimizations.

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -11,7 +11,8 @@ defmodule Hamming do
   def hamming_distance(a, b) when length(a) != length(b), do: {:error, "Lists must be the same length"}
   def hamming_distance(a, b), do: {:ok, calculate_distance(a, b)}
 
-  defp calculate_distance([], []), do: 0
-  defp calculate_distance([a | tail_a], [a | tail_b]), do: calculate_distance(tail_a, tail_b)
-  defp calculate_distance([a | tail_a], [b | tail_b]), do: 1 + calculate_distance(tail_a, tail_b)
+  defp calculate_distance(a, b, acc \\ 0)
+  defp calculate_distance([], [], acc), do: acc
+  defp calculate_distance([a | tail_a], [a | tail_b], acc), do: calculate_distance(tail_a, tail_b, acc)
+  defp calculate_distance([a | tail_a], [b | tail_b], acc), do: calculate_distance(tail_a, tail_b, acc + 1)
 end
