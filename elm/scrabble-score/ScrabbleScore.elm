7ee422b33f6ccadebc694e428ee14e64fcd5f527
Christian Wesselhoeft <hi@xtian.us>
2018-01-12 15:22:28 -0700

    Complete Elm scrabble-score

---
+++ elm/scrabble-score/ScrabbleScore.elm
@@ -0,0 +1,29 @@
+module ScrabbleScore exposing (scoreWord)
+
+import Dict exposing (Dict)
+
+
+scoreWord : String -> Int
+scoreWord =
+    String.toUpper
+        >> String.toList
+        >> List.filterMap (flip Dict.get scores)
+        >> List.sum
+
+
+scores : Dict Char Int
+scores =
+    let
+        assignScores score letters dict =
+            letters
+                |> String.toList
+                |> List.foldl (flip Dict.insert score) dict
+    in
+        Dict.empty
+            |> assignScores 1 "AEIOULNRST"
+            |> assignScores 2 "DG"
+            |> assignScores 3 "BCMP"
+            |> assignScores 4 "FHVWY"
+            |> assignScores 5 "K"
+            |> assignScores 8 "JX"
+            |> assignScores 10 "QZ"
