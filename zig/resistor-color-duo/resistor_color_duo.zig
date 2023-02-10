ace55f788e8a47552371dc5926bd1dd4e87a1497
Christian Wesselhoeft <hi@xtian.us>
2023-02-09 22:23:10 -0700

    Improve resistor-color-duo

--- zig/resistor-color-duo/resistor_color_duo.zig
+++ zig/resistor-color-duo/resistor_color_duo.zig
@@ -1,6 +1,5 @@
 pub const ColorBand = enum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
 
 pub fn colorCode(colors: []const ColorBand) !isize {
-    const first: isize = @enumToInt(colors[0]);
-    return if (colors.len == 1) first else first * 10 + @enumToInt(colors[1]);
+    return 10 * @as(isize, @enumToInt(colors[0])) + @enumToInt(colors[1]);
 }
