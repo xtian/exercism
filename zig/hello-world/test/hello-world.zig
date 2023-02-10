6ac940a47224d8dd748c3cb4073d66392eb9b1e5
Christian Wesselhoeft <hi@xtian.us>
2023-02-09 18:35:02 -0700

    Complete new Zig hello-world

--- zig/hello-world/test/hello-world.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const testing = std.testing;
-
-const hello_world = @import("../src/main.zig");
-
-test "says hello" {
-    testing.expect(std.mem.eql(u8, hello_world.hello(), "Hello, World!"));
-}
