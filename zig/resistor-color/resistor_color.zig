e3b2bc589131d3219f362878a757fb7cf8367ef0
Christian Wesselhoeft <hi@xtian.us>
2023-02-09 21:52:15 -0700

    Complete zig resistor-color

---
+++ zig/resistor-color/resistor_color.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub const ColorBand = enum { black, brown, red, orange, yellow, green, blue, violet, grey, white };
+
+pub fn colorCode(color: ColorBand) isize {
+    return @enumToInt(color);
+}
+
+pub fn colors() []const ColorBand {
+    return std.enums.values(ColorBand);
+}
