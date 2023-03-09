cffd553636231ecd9641fc60b50ddc0ffc4d72d2
Christian Wesselhoeft <hi@xtian.us>
2023-03-09 09:44:51 -0700

    Improve Zig difference-of-squares

--- zig/difference-of-squares/difference_of_squares.zig
+++ zig/difference-of-squares/difference_of_squares.zig
@@ -1,21 +1,10 @@
 pub fn squareOfSum(number: usize) usize {
-    var sum: usize = 0;
-
-    for (1..number + 1) |i| {
-        sum += i;
-    }
-
+    const sum = number * (number + 1) / 2;
     return sum * sum;
 }
 
 pub fn sumOfSquares(number: usize) usize {
-    var sum: usize = 0;
-
-    for (1..number + 1) |i| {
-        sum += i * i;
-    }
-
-    return sum;
+    return number * (number + 1) * (2 * number + 1) / 6;
 }
 
 pub fn differenceOfSquares(number: usize) usize {
